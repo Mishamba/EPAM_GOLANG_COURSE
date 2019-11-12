8aeab0cf725614309d4b452d96897ec2fa6cc88c
Mishamba <misha.nenahov@gmail.com>
2019-11-12 22:00:19 +0300

    finished first hometask

---
+++ go/src/hello_world.go
@@ -0,0 +1,9 @@
+//i'll left some comments. correct me if i was wrong somewhere 
+
+package main //this line is name of package
+
+import "fmt" //in this line i conect some another packeges, that i need to write my programm
+
+func main() {
+	fmt.Println("hello world")
+}
