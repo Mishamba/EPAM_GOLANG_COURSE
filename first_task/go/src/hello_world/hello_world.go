0b1c360fb0d1ace4bbd9f98c68ec208750b985b1
Mishamba <misha.nenahov@gmail.com>
2019-11-13 15:56:43 +0300

    trynig to create pull request(i have problems here)

--- first_task/go/src/hello_world/hello_world.go
+++ first_task/go/src/hello_world/hello_world.go
@@ -1,8 +1,6 @@
-//i'll left some comments. correct me if i was wrong somewhere 
+package main //this line is name of package?
 
-package main //this line is name of package
-
-import "fmt" //in this line i conect some another packeges, that i need to write my programm
+import "fmt" //in this line i conect some another packeges, that i need to write my programm?
 
 func main() {
 	fmt.Println("hello world...")
